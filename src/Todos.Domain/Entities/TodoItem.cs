cfbdc34544fb023d6eca2f5247ab729278dbc218
itzJustKranker <me@michaelkranker.com>
2021-08-11 16:45:35 -0400

    item priority migration

--- src/Todos.Domain/Entities/TodoItem.cs
+++ src/Todos.Domain/Entities/TodoItem.cs
@@ -7,6 +7,7 @@ namespace Todos.Domain.Entities
         public string Title { get; set; }
         public string Description { get; set; }
         public bool Completed { get; set; }
+        public int Priority { get; set; }
 
         public int ListId { get; set; }
         public virtual TodoList List { get; set; }
