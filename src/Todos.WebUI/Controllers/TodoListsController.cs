647f33762b7c6aa2445e25060ab7c52350c553db
itzJustKranker <me@michaelkranker.com>
2021-08-18 20:19:22 -0400

    add override getall for context

--- src/Todos.WebUI/Controllers/TodoListsController.cs
+++ src/Todos.WebUI/Controllers/TodoListsController.cs
@@ -1,3 +1,5 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Todos.Application.Interfaces;
 using Todos.Domain.Entities;
 
@@ -5,8 +7,18 @@ namespace Todos.WebUI.Controllers
 {
     public class TodoListsController : ApiCrudControllerBase<TodoList>
     {
+        private readonly ITodoListRepository _repository;
+        
         public TodoListsController(ITodoListRepository repository) : base(repository)
         {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAll()
+        {
+            var allLists = await _repository.GetAllAsync();
+            return new OkObjectResult(allLists);
         }
     }
 }
