c0712728fc8ebb50d984b1639ea409bbf9cbe8c9
itzJustKranker <me@michaelkranker.com>
2022-01-30 21:10:37 -0500

    added database settings

---
+++ src/Todos.Infrastructure/Settings/DatabaseSettings.cs
@@ -0,0 +1,7 @@
+namespace Todos.Infrastructure.Settings
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
--- src/Todos.WebUI/Startup.cs
+++ src/Todos.WebUI/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using Todos.Infrastructure;
+using Todos.Infrastructure.Settings;
 
 namespace Todos.WebUI
 {
@@ -27,6 +28,9 @@ namespace Todos.WebUI
                 );
 
             services.AddInternalServices();
+
+            services.Configure<DatabaseSettings>(options => 
+                Configuration.GetSection(nameof(DatabaseSettings)).Bind(options));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
